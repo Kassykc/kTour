8fcc100696f1fcbc4dc77c01c5df59d316bd3bc6
Sang Yee Park <design04@hicomp.net>
2025-04-23 19:50:48 +0900

    푸터퍼블

--- web-src/components/common/Footer.vue
+++ web-src/components/common/Footer.vue
@@ -4,32 +4,28 @@
     <div id="footer" class="bg-[#001181]">
       <div id="footer_content" class="w-full max-w-[1200px] h-[535px] md:h-[354px] ml-[0] mr-[auto] sm:mx-auto min-w-[320px] min-h-[354px] sm:max-h-[354px] px-[28px] sm:px-[18px]">
         <address class="flex justify-center flex-col sm:justify-around sm:flex-row lg:justify-between items-start sm:items-center h-[535px] sm:h-[354px] min-h-[354px] gap-[10px]">
-          <div class="brand_info flex flex-col gap-[12px] items-start sm:items-center sm:items-start">
-            <img src="/img/common/logo/footer_logo.png" class="w-[90px] h-auto min-w-[90px] md:w-[90px]">
-            <div class="business_info text-[14px]">
-              CEO. Lee Ji Sun (Steve Lee)<br/>
-              Registration No. 395-81-01930
+          <div class="brand_info flex flex-col gap-[24px] items-start">
+            <img src="/img/common/logo/footer_logo.png" class="w-[90px] h-auto min-w-[90px] md:w-[220px]">
+            <div class="business_info text-[14px] font-[500] text-white leading-[23px]">
+              <span class="font-[700]">대표자 : 박종인 (Sunny / Eunheui Suh)</span><br/>
+              경기도 고양시 일산동구 무궁화로 43-55, 302호 2715,<br/>
+              Hyundai41Tower, 293 Mokdongdong-ro, Yangcheon-gu, Seoul
             </div>
-            <div class="rights text-[16px] font-[600] ">
-              © 2025 able. All rights reserved.
+            <div class="rights text-[13px] font-[400] text-white">
+              Copyright Medicity. All rights reserved.
             </div>
           </div>
-          <div class="company_info flex flex-col sm:gap-[20px] lg:flex-row lg:gap-[46px] lg:mt-[75px]">
-            <div class="contact flex flex-col gap-[0px] lg:gap-[18px] items-start sm:items-center sm:items-start">
-              <div class="phone text-[20px] sm:text-[28px]">1599.6956</div>
-              <div class="email text-[16px] sm:text-[18px] font-[700]">support@hicomp.net</div>
+          <div class="company_info flex flex-col gap-[20px] lg:gap-[30px]">
+            <div class="sns flex gap-[32px] justify-end items-center">
+              <div class="facebook"><a href="/" target="_blank"><img src="@/assets/images/facebook.png"></a></div>
+              <div class="twitter"><a href="/" target="_blank"><img src="@/assets/images/twitter.png"></a></div>
+              <div class="instagram"><a href="/" target="_blank"><img src="@/assets/images/instagram.png"></a></div>
             </div>
-            <div class="head_office text-[14px] text-[#9D9D9D] mt-[36px] mb-[10px] sm:mt-[0px] sm:mb-[0px]">
-              <b class="text-[16px] font-[700]">Head Office</b><br/>
-              ROOM 15A(202), 2NDFLOOR,6,<br/>
-              EONJU-RO 134-GIL, GANGNAM-GU,<br/>
-              SEOUL, REPUBLIC OF KOREA
-            </div>
-            <div class="research_center text-[14px] text-[#9D9D9D]">
-              <b class="text-[16px] font-[700]">Research Center</b><br/>
-              43-55, Mugunghwa-ro, Ilsandong-<br/>
-              gu, Goyang-si, Gyeonggi-do, Room<br/>
-              302-304, Sungwoo Sakar Tower
+            <div class="contact flex  gap-[0px] lg:gap-[20px] items-start sm:items-center justify-end text-white">
+              <div class="email text-[12px] sm:text-[14px] font-[400]"><span class="font-[700]">E-mail</span>ogago6747@naver.com</div>
+              <div class="tell text-[12px] sm:text-[14px] font-[400]"><span class="font-[700]">Tel</span>+82-2-2647-6747</div>
+              <div class="fax text-[12px] sm:text-[14px] font-[400]"><span class="font-[700]">Fax</span>+82-50-4019-6747</div>
+              <div class="policy text-[12px] sm:text-[14px] font-[700]">개인정보처리방침</div>
             </div>
           </div>
         </address>
