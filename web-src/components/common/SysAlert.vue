d488892b1d2bf6a91d426813ac1943ffbeaaf1d4
DESKTOP-FDUQSC8\dev <prog02@hicomp.net>
2025-04-24 11:27:16 +0900

    admin set

--- web-src/components/common/SysAlert.vue
+++ web-src/components/common/SysAlert.vue
@@ -1,14 +1,14 @@
 <template>
-    <div v-if="show" class="common_sys_alert_modal">
+    <div v-if="show" class="common_sys_alert_modal fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center z-[9999]">
         <!-- 배경 클릭 시 모달이 닫히지 않도록 설정 -->
-        <div class="modal_overlay" @click="handleOverlayClick"></div>
-        <div class="modal_content" @click.stop>
+        <div class="modal_overlay absolute top-0 left-0 right-0 bottom-0 bg-black/50" @click="handleOverlayClick"></div>
+        <div class="modal_content flex flex-col w-[70vw] max-w-[500px] !pt-[4rem] !px-[2rem] !pb-[2rem] bg-white rounded-[8px] text-center relative" @click.stop>
             <div v-if="isHtml">{{ message }}</div>
             <div v-if="!isHtml" v-html="message"></div>
-            <div class="modal_buttons">
-                <div @click="handleOk" class="btn ok">{{ okText }}</div>
+            <div class="modal_buttons flex justify-center items-center flex-col gap-[0.5rem] !mt-auto !pt-[3rem]">
+                <div @click="handleOk" class="btn ok flex justify-center items-center w-full h-[50px] rounded-[25px] text-[0.85em] font-[700] cursor-pointer text-white bg-[#5e0088]">{{ okText }}</div>
                 <!-- confirm일 때만 cancel 버튼을 추가 -->
-                <div v-if="cancelText" @click="handleCancel" class="btn cancel">{{ cancelText }}</div>
+                <div v-if="cancelText" @click="handleCancel" class="btn cancel flex justify-center items-center w-full h-[50px] rounded-[25px] text-[0.85em] font-[700] cursor-pointer text-[#5e0088] border border-[#5e0088] bg-white">{{ cancelText }}</div>
             </div>
         </div>
     </div>
@@ -64,61 +64,61 @@ const handleCancel = () => {
 
 <style lang="scss" scoped>
 .common_sys_alert_modal {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 9999;
+    // position: fixed;
+    // top: 0;
+    // left: 0;
+    // right: 0;
+    // bottom: 0;
+    // display: flex;
+    // justify-content: center;
+    // align-items: center;
+    // z-index: 9999;
 
     .modal_overlay {
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: rgba(0, 0, 0, 0.5);
+        // position: absolute;
+        // top: 0;
+        // left: 0;
+        // right: 0;
+        // bottom: 0;
+        // background-color: rgba(0, 0, 0, 0.5);
     }
 
     .modal_content {
-        display: flex;
-        flex-direction: column;
-        width: 70vw;
-        max-width: 500px;
-        padding: 4rem 2rem 2rem;
-        background-color: white;
-        border-radius: 8px;
-        text-align: center;
-        position: relative;
+        // display: flex;
+        // flex-direction: column;
+        // width: 70vw;
+        // max-width: 500px;
+        // padding: 4rem 2rem 2rem;
+        // background-color: white;
+        // border-radius: 8px;
+        // text-align: center;
+        // position: relative;
 
         .modal_buttons {
-            @include flexCenter;
-            flex-direction: column;
-            gap: 0.5rem;
-            margin-top: auto;
-            padding-top: 3rem;
+            // @include flexCenter;
+            // flex-direction: column;
+            // gap: 0.5rem;
+            // margin-top: auto;
+            // padding-top: 3rem;
 
             .btn {
-                @include flexCenter;
-                width: 100%;
-                height: 50px;
-                border-radius: 25px;
-                font-size: 0.85em;
-                font-weight: 700;
-                cursor: pointer;
+                // @include flexCenter;
+                // width: 100%;
+                // height: 50px;
+                // border-radius: 25px;
+                // font-size: 0.85em;
+                // font-weight: 700;
+                // cursor: pointer;
 
                 &.ok {
-                    color: $color-white-000;
-                    background: $art_buddy_btn_purple;
+                    // color: $color-white-000;
+                    // background: $art_buddy_btn_purple;
                 }
 
                 &.cancel {
-                    color: $art_buddy_btn_purple;
-                    background: $color-white-000;
-                    border: 1px solid $art_buddy_btn_purple;
+                    // color: $art_buddy_btn_purple;
+                    // background: $color-white-000;
+                    // border: 1px solid $art_buddy_btn_purple;
                 }
             }
         }
--- web-src/components/layer/Popup.vue
+++ web-src/components/layer/Popup.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="popups.length > 0">
-        <div v-for="popup in popups" :key="popup.id" class="popup-backdrop">
-            <div class="popup-content" @click.stop>
+        <div v-for="popup in popups" :key="popup.id" class="popup-backdrop fixed top-0 left-0 z-[999] w-full h-full bg-black/50 flex justify-center items-center">
+            <div class="popup-content flex justify-center items-center sm:block fixed top-1/2 left-1/2 z-[999] w-[100vw] sm:w-[80vw] max-w-[1100px] h-[100vh] max-h-[100vh] sm:h-auto sm:max-h-[80vh] text-center bg-white rounded-0 sm:rounded-[8px] shadow-[0_2px_10px_rgba(0,0,0,0.1)] overflow-y-scroll sm:overflow-y-auto -translate-x-1/2 -translate-y-1/2 !pt-[100px] !pb-[10px] !pl-[20px] !pr-[10px] sm:!p-[20px]" @click.stop>
                 <component :is="popup.component" v-bind="popup.props" @close="close(popup.id)" />
             </div>
         </div>
@@ -19,32 +19,4 @@ const close = (id: number) => {
 </script>
 
 <style lang="scss" scoped>
-.popup-backdrop {
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 998;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.5);
-    // display: flex;
-    // justify-content: center;
-    // align-items: center;
-
-    .popup-content {
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        z-index: 999;
-        width: 80vw;
-        max-width: 500px;
-        max-height: 80vh;
-        text-align: center;
-        background: white;
-        border-radius: 8px;
-        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-        overflow-y: auto;
-        transform: translate(-50%, -50%);
-    }
-}
 </style>
