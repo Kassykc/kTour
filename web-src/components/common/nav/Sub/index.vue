0966023025978c71ad59da5d9b32e2de863558aa
DESKTOP-FDUQSC8\dev <prog02@hicomp.net>
2025-05-09 15:03:27 +0900

    메뉴 다국어 적용 및 수정사항 적용

--- web-src/components/common/nav/Sub/index.vue
+++ web-src/components/common/nav/Sub/index.vue
@@ -1,24 +1,20 @@
 <template>
-  <div
-    v-if="visible && activeNav !== null && submenus[activeNav]"
-    class="absolute top-[86px] left-0 w-screen z-50 bg-white/90"
-    @mouseenter="$emit('submenu-enter')"
-    @mouseleave="$emit('submenu-leave')"
-    >
-    <div class="px-[20px] flex justify-start items-center w-full mx-auto min-w-[320px] h-[250px] md:h-[280px] lg:h-[320px]">
-      <div class="submenu_title_area w-[28%] h-full pr-[100px] font-[700] text-[34px] border-r border-[#ADADAD] flex items-center justify-end bg-[url('/img/common/nav/submenu_bg.png')] bg-[50%] bg-[position:75%_center] bg-no-repeat text-right" v-html="menus[activeNav]?.title">
+  <div v-if="visible && activeNav !== null && submenus[activeNav]"
+    class="absolute top-[86px] left-0 w-screen z-50 bg-white/90" @mouseenter="$emit('submenu-enter')"
+    @mouseleave="$emit('submenu-leave')">
+    <div
+      class="px-[20px] flex justify-start items-center w-full mx-auto min-w-[320px] h-[250px] md:h-[280px] lg:h-[320px]">
+      <div
+        class="submenu_title_area w-[28%] h-full pr-[100px] font-[700] text-[34px] border-r border-[#ADADAD] flex items-center justify-end bg-[url('/img/common/nav/submenu_bg.png')] bg-[50%] bg-[position:75%_center] bg-no-repeat text-right"
+        v-html="menus[activeNav]?.title">
       </div>
-      <div class="submenu_area flex flex-wrap items-center min-w-[250px] w-auto h-[180px] pl-[20px] gap-x-[8px] gap-y-[4px]">
-        <div
-          v-for="(item, idx) in submenus[activeNav]"
-          :key="idx"
-          class="w-[48%] text-left"
-        >
+      <div
+        class="submenu_area flex flex-wrap items-center min-w-[250px] w-auto h-[180px] pl-[20px] gap-x-[8px] gap-y-[4px]">
+        <div v-for="(item, idx) in submenus[activeNav]" :key="idx" class="w-[48%] text-left">
           <div
             class="text-black cursor-pointer hover:text-[#F57E1F] text-[13px] md:text-[14px] lg:text-[20px] font-[700] px-[10px]"
-            @click="router.push(item.url)"
-          >
-            {{ item.title }}
+            @click="router.push(item.url)">
+            {{ t(item.title) }}
           </div>
         </div>
       </div>
@@ -27,7 +23,7 @@
 </template>
 
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
+import { t } from '@/plugins/i18n'
 
 const props = defineProps<{
   visible: boolean
@@ -37,44 +33,43 @@ const props = defineProps<{
 const router = useRouter()
 
 const menus = [
-  { title: 'About Us' },
-  { title: 'Medical<br/>Treatment Service' },
-  { title: 'Medi Tour' },
-  { title: 'Reviews' },
-  { title: 'Shop' },
-  { title: 'Inquiry' },
+  { title: 'menu.1.name', url: '/' },
+  { title: 'menu.2.name', url: '/' },
+  { title: 'menu.3.name', url: '/' },
+  // { title: 'menu.4.name', url: '/' },
+  // { title: 'menu.5.name', url: '/' },
+  { title: 'menu.6.name', url: '/' },
 ]
 
 // 각 nav에 대응하는 서브메뉴 데이터
 const submenus = [
   [
-    { title: 'About Company', url: '/' },
-    { title: 'Why Korea?', url: '/' },
-    { title: 'About Service - Medi-Tour Program', url: '/' },
-    { title: 'Why Medi-Tour?', url: '/' }
+    { title: 'menu.1.2depth.1.name', url: '/' },
+    { title: 'menu.1.2depth.2.name', url: '/' },
+    { title: 'menu.1.2depth.3.name', url: '/' },
+    { title: 'menu.1.2depth.4.name', url: '/' }
   ],
   [
-    { title: '업무분야', url: '/' },
-    { title: 'PORTFOLIO', url: '/' }
+    { title: 'menu.2.2depth.1.name', url: '/' },
+    { title: 'menu.2.2depth.2.name', url: '/' },
   ],
   [
-    { title: '서비스1', url: '/' },
-    { title: '서비스2', url: '/' }
+    { title: 'menu.3.2depth.1.name', url: '/' },
+    { title: 'menu.3.2depth.2.name', url: '/' },
   ],
+  // [
+  //   { title: '프로젝트1', url: '/' },
+  //   { title: '프로젝트2', url: '/' }
+  // ],
+  // [
+  //   { title: '프로젝트1', url: '/' },
+  //   { title: '프로젝트2', url: '/' }
+  // ],
   [
-    { title: '프로젝트1', url: '/' },
-    { title: '프로젝트2', url: '/' }
-  ],
-  [
-    { title: '공지사항', url: '/' },
-    { title: '견적/상담문의', url: '/' },
-    { title: '연구비 및 사업공고', url: '/' },
-    { title: '기업관련 공고', url: '/' },
-    { title: '고객센터', url: '/' }
-  ],
-  [
-    { title: 'Inquiry1', url: '/' },
-    { title: 'Inquiry2', url: '/' }
+    { title: 'menu.6.2depth.1.name', url: '/' },
+    { title: 'menu.6.2depth.2.name', url: '/' },
+    { title: 'menu.6.2depth.3.name', url: '/' },
+    { title: 'menu.6.2depth.4.name', url: '/' }
   ],
 ]
 </script>
--- web-src/components/common/nav/index.vue
+++ web-src/components/common/nav/index.vue
@@ -1,22 +1,16 @@
 <template>
     <div class="flex h-full gap-x-10 min-w-[250px] h-full">
-        <div
-        v-for="(item, index) in menus"
-        :key="index"
-        class="flex items-center justify-center cursor-pointer text-[1rem] pt-[5px] h-full border-b-[3px] font-[600]"
-        @mouseenter="$emit('hover', index)"
-        @mouseleave="$emit('leave')"
-        :class="[
-        activeNav === index
-            ? 'text-[#F57E1F] border-[#F57E1F]'
-            : isHoveredHeader
-                ? 'text-black border-transparent'
-                : 'text-white border-transparent',
-            'hover:text-[#F57E1F] hover:border-[#F57E1F]'
-        ]"
-        @click="$emit('select', index)"
-        >
-        {{ t(item.titlePath) }}
+        <div v-for="(item, index) in menus" :key="index"
+            class="flex items-center justify-center cursor-pointer text-[1rem] pt-[5px] h-full border-b-[3px] font-[600]"
+            @mouseenter="$emit('hover', index)" @mouseleave="$emit('leave')" :class="[
+                activeNav === index
+                    ? 'text-[#F57E1F] border-[#F57E1F]'
+                    : isHoveredHeader
+                        ? 'text-black border-transparent'
+                        : 'text-white border-transparent',
+                'hover:text-[#F57E1F] hover:border-[#F57E1F]'
+            ]" @click="$emit('select', index)">
+            {{ t(item.titlePath) }}
         </div>
     </div>
 </template>
@@ -32,11 +26,11 @@ defineProps<{
 defineEmits(['select'])
 
 const menus = [
-    { titlePath: 'menu.1', url: '/' },
-    { titlePath: 'menu.2', url: '/' },
-    { titlePath: 'menu.3', url: '/' },
-    { titlePath: 'menu.4', url: '/' },
-    { titlePath: 'menu.5', url: '/' },
-    { titlePath: 'menu.6', url: '/' },
+    { titlePath: 'menu.1.name', url: '/' },
+    { titlePath: 'menu.2.name', url: '/' },
+    { titlePath: 'menu.3.name', url: '/' },
+    // { titlePath: 'menu.4.name', url: '/' },
+    // { titlePath: 'menu.5.name', url: '/' },
+    { titlePath: 'menu.6.name', url: '/' },
 ]
 </script>
