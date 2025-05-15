cd8cf41c9f6b801dd4e09eb00d87c180ae638c80
Sang Yee Park <design04@hicomp.net>
2025-05-15 10:53:19 +0900

    +

--- web-src/pages/tour/seoul/index.vue
+++ web-src/pages/tour/seoul/index.vue
@@ -6,13 +6,14 @@
             <div id="seoul_tour">
                 <div class="main_title w-full max-w-[1340px] pt-[160px] pb-[120px] my-0 mx-auto">
                     <h2 class="area_name text-[65px] font-[700] mb-[120px]">
-                        {{ t('about.part2.title') }}
+                        {{ t('seoulTour.title') }}
                     </h2>
                     <p class="description text-[40px] font-[700] mb-[34px]">
-                        {{ t('about.part2.content1.title') }}
+                        {{ t('seoulTour.subtitle') }}
                     </p>
                     <p class="text-[24px] mb-[86px]">
-                        {{ t('about.part2.content1.desc1') }}
+                        {{ t('seoulTour.desc1') }}<br/>
+                        {{ t('seoulTour.desc2') }}
                     </p>
                 
                 </div>
