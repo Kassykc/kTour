17a457d676452fac354133532ac602199fdce3aa
DESKTOP-FDUQSC8\dev <prog02@hicomp.net>
2025-05-15 10:34:59 +0900

    +

--- web-src/pages/about/index.vue
+++ web-src/pages/about/index.vue
@@ -1,16 +1,6 @@
 <template>
     <div>
-<<<<<<< HEAD
-        <div class="banner_area w-full h-[435px] bg-[url('@/assets/images/sub/banner_bg.png')] bg-cover bg-center">
-            <div
-                class="title_area max-w-[1340px] w-full h-[435px] flex flex-col justify-center items-center my-0 mx-auto">
-                <div class="title text-white font-[700] text-[80px] mt-[100px]">{{ t('menu.1.name') }}</div>
-                <SubTab v-model="selectedTab" category="about" />
-            </div>
-        </div>
-=======
         <Banner :title="bannerTitle" :bgImage="bannerBgImage" :category="category" :selectedTab="selectedTab" />
->>>>>>> 881ec4dc696f5e4603b4360e23b3abf16ea0c425
 
         <div class="content_area">
             <div id="company">
