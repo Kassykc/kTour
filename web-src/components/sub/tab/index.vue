fefc39dcc08e430a4c0f917d52415e9f1071945a
DESKTOP-FDUQSC8\dev <prog02@hicomp.net>
2025-05-15 10:32:21 +0900

    +

--- web-src/components/sub/tab/index.vue
+++ web-src/components/sub/tab/index.vue
@@ -1,24 +1,19 @@
 <template>
     <div>
         <div class="sub_tab_area flex gap-[30px]">
-            <div
-                v-for="tab in filteredTabs"
-                :key="tab.key"
-                class="sub_tab cursor-pointer py-[10px] text-[20px] border-b"
-                :class="{
-                'text-white border-white': activeTab === tab.key,
-                'text-[#FFFFFF99] border-transparent': activeTab !== tab.key
-                }"
-                @click="handleClick(tab.key)"
-            >
-                {{ tab.label }}
+            <div v-for="tab in filteredTabs" :key="tab.key"
+                class="sub_tab cursor-pointer py-[10px] text-[20px] border-b" :class="{
+                    'text-white border-white': activeTab === tab.key,
+                    'text-[#FFFFFF99] border-transparent': activeTab !== tab.key
+                }" @click="handleClick(tab.key)">
+                {{ t(tab.label) }}
             </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { defineProps, defineEmits, ref, watch, computed } from 'vue'
+import { t } from '@/plugins/i18n'
 
 const props = defineProps<{
     modelValue: string
@@ -29,16 +24,16 @@ const emit = defineEmits(['update:modelValue'])
 
 const allTabs = {
     about: [
-        { key: 'company', label: 'Company' },
-        { key: 'service', label: 'Service' },
-        { key: 'korea', label: 'Why Korea' },
-        { key: 'meditour', label: 'K-MediTour' },
+        { key: 'company', label: 'tabs.about.company' },
+        { key: 'service', label: 'tabs.about.service' },
+        { key: 'korea', label: 'tabs.about.korea' },
+        { key: 'meditour', label: 'tabs.about.meditour' },
     ],
     // 페이지 이름, 메뉴명 추가 및 변경 해야 됨
     info: [
-        { key: 'info', label: 'Info' },
-        { key: 'text', label: 'Text' },
-        { key: 'login', label: 'Login' },
+        { key: 'info', label: 'tabs.about.info' },
+        { key: 'text', label: 'tabs.about.text' },
+        { key: 'login', label: 'tabs.about.login' },
     ]
 }
 
@@ -61,5 +56,4 @@ const handleClick = (key: string) => {
 }
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- web-src/pages/about/index.vue
+++ web-src/pages/about/index.vue
@@ -3,7 +3,7 @@
         <div class="banner_area w-full h-[435px] bg-[url('@/assets/images/sub/banner_bg.png')] bg-cover bg-center">
             <div
                 class="title_area max-w-[1340px] w-full h-[435px] flex flex-col justify-center items-center my-0 mx-auto">
-                <div class="title text-white font-[700] text-[80px] mt-[100px]">About Us</div>
+                <div class="title text-white font-[700] text-[80px] mt-[100px]">{{ t('menu.1.name') }}</div>
                 <SubTab v-model="selectedTab" category="about" />
             </div>
         </div>
