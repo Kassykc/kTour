7244a2083c43b583c23da21a24691a916d9fed83
Sang Yee Park <design04@hicomp.net>
2025-05-19 14:29:53 +0900

    메인 반응형 진행중

--- web-src/components/main/banner/index.vue
+++ web-src/components/main/banner/index.vue
@@ -7,14 +7,14 @@
         </video>
 
         <img src="/img/main/banner_logo.png" alt="배너로고"
-            class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-auto h-auto max-w-[60%] sm:max-w-[30%] pb-[10vh]" />
+            class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-auto h-auto max-w-[40%] sm:max-w-[30%] pb-[10vh]" />
 
         <div
-            class="relative z-20 flex flex-col sm:gap-[28px] justify-center items-start w-[320px] sm:w-full max-w-[1340px] ml-[20px] mr-auto sm:mx-auto min-w-[320px] px-[28px] sm:px-[18px] h-full text-white">
-            <div class="sub_title text-[1.7rem] font-[500] text-left">
+            class="relative z-20 flex flex-col sm:gap-[28px] justify-center items-center sm:items-start w-full sm:w-[320px] sm:w-full max-w-[1340px] ml-[20px] mr-auto sm:mx-auto min-w-[320px] px-[28px] sm:px-[18px] h-full text-white">
+            <div class="sub_title text-[20px] md:text-[24px] lg:text-[30px] font-[500] text-center sm:text-left">
                 {{ t("main.banner.text1") }}
             </div>
-            <div class="main_title text-[75px] text-left break-keep leading-[85px] font-[700]">
+            <div class="main_title text-[24px] md:text-[40px] lg:text-[50px] xl:text-[75px] text-left break-keep md:leading-[45px] lg:leading-[55px] xl:leading-[85px] font-[700]  text-center sm:text-left">
                 {{ t("main.banner.text2") }} <br />
                 {{ t("main.banner.text3") }} <br />
                 {{ t("main.banner.text4") }}
