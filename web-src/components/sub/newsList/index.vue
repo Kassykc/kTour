326ca97d8ca59fa2c841f89de13e86efa79cf101
Sang Yee Park <design04@hicomp.net>
2025-05-20 17:14:39 +0900

    뉴스 페이지 반응형 퍼블

--- web-src/components/sub/newsList/index.vue
+++ web-src/components/sub/newsList/index.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
-        <div class="news_list_area w-full max-w-[1340px] mx-auto flex justify-start items-start pb-[270px] gap-x-[25px] gap-y-[25px] flex-wrap">
+        <div class="news_list_area w-full max-w-[1380px] px-[20px] mx-auto flex flex-col sm:flex-row justify-center sm:justify-start items-start pb-[270px] gap-x-[25px] gap-y-[25px] flex-wrap">
             <!-- v-for로 news_list 반복 -->
-            <div v-for="(item, index) in newsList" :key="index" class="new_list w-[428px] h-[528px] shadow-[0_4px_15px_rgba(0,0,0,0.2)]">
+            <div v-for="(item, index) in newsList" :key="index" class="new_list w-full sm:w-[428px] h-[528px] shadow-[0_4px_15px_rgba(0,0,0,0.2)]">
                 <div class="img_wrap w-full h-[320px]">
                     <img :src="item.img" alt="" class="w-full h-full object-cover">
                 </div>
 
-                <div class="cont_area py-[20px] px-[30px] flex flex-col justify-between items-start h-[208px]">
-                    <div class="title w-full h-[70px] text-[#2A2A2A] text-[24px] font-[600] overflow-hidden 
+                <div class="cont_area p-[15px] sm:py-[20px] sm:px-[30px] flex flex-col justify-between items-start h-[208px]">
+                    <div class="title w-full h-[60px] sm:h-[70px] text-[#2A2A2A] text-[18px] sm:text-[24px] font-[600] overflow-hidden 
                         text-ellipsis block -webkit-line-clamp-2 -webkit-box-orient-vertical">
                         {{ item.title }}
                     </div>
