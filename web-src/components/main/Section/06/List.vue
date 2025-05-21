a2c1064edd5fca5d3a273fd9af94614e711a2b8f
Sang Yee Park <design04@hicomp.net>
2025-05-21 13:19:54 +0900

    메인 리스트 반응형 수정

--- web-src/components/main/Section/06/List.vue
+++ web-src/components/main/Section/06/List.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="flex flex-col justify-center w-full px-[20px] xl:px-0">
         <div v-for="(item, index) in props.list" :key="index" class="flex flex-col items-start w-full">
-            <div class="flex justify-between items-center w-full py-[20px] lg:py-[30px] border-b border-black">
-                <div class="text-[20px] lg:text-[26px] font-[400] pl-2 w-[250px] sm:w-[400px] lg:w-fit truncate">
+            <div class="flex flex-col sm:flex-row justify-between items-start sm:items-center w-full py-[20px] lg:py-[30px] border-b border-black">
+                <div class="text-[20px] lg:text-[26px] font-[400] pl-2 w-full sm:w-[400px] lg:w-fit h-[60px] sm:h-auto line-clamp-2">
                     {{ item.title }}
                 </div>
                 <div class="pt-2 pr-2">
