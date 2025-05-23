c039f108248548da727e0882770f9a216e05042f
Sang Yee Park <design04@hicomp.net>
2025-05-23 16:53:00 +0900

    파트너 로고 추가

--- web-src/components/main/Section/Spon/index.vue
+++ web-src/components/main/Section/Spon/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <div class="flex flex-col items-center h-full bg-[#fff] py-[100px] ">
+        <div class="flex flex-col items-center h-full py-[100px] ">
+            <!-- bg-[#FAFAFA] -->
             <div class="flex flex-col items-start w-full max-w-[1340px] px-[20px] pb-2 lg:pb-10">
                 <div class="flex flex-col justify-center items-start">
                     <MainSectionLinkTitle :title="t('main.section6.part1.title')" :color="'#7A5083'" />
@@ -21,8 +22,8 @@
                     }"
                     class="w-full max-w-[1920px] flex justify-center items-center">
                         <SwiperSlide v-for="(item, idx) in spon" :key="idx">
-                            <div class="text-center flex justify-center items-center h-[60px] shrink-0">
-                                <img :src="item.img" class="shrink-0" />
+                            <div class="text-center flex justify-center items-center w-[130px] h-[100px] sm:w-[200px] sm:h-[150px] mx-auto">
+                                <img :src="item.img" class="w-full object-contain" />
                             </div>
                         </SwiperSlide>
                     </Swiper>
@@ -43,9 +44,20 @@ import spon02 from '@/assets/images/main/spon/02.png'
 import spon03 from '@/assets/images/main/spon/03.png'
 import spon04 from '@/assets/images/main/spon/04.png'
 import spon05 from '@/assets/images/main/spon/05.png'
-import spon06 from '@/assets/images/main/spon/06.png'
+import spon06 from '@/assets/images/main/spon/06.jpg'
 import spon07 from '@/assets/images/main/spon/07.png'
 import spon08 from '@/assets/images/main/spon/08.png'
+import spon09 from '@/assets/images/main/spon/09.png'
+import spon10 from '@/assets/images/main/spon/10.png'
+import spon11 from '@/assets/images/main/spon/11.png'
+import spon12 from '@/assets/images/main/spon/12.png'
+import spon13 from '@/assets/images/main/spon/13.png'
+import spon14 from '@/assets/images/main/spon/14.png'
+import spon15 from '@/assets/images/main/spon/15.png'
+import spon16 from '@/assets/images/main/spon/16.png'
+import spon17 from '@/assets/images/main/spon/17.png'
+import spon18 from '@/assets/images/main/spon/18.png'
+import spon19 from '@/assets/images/main/spon/19.png'
 
 const spon = [
     { img: spon01 },
@@ -56,6 +68,17 @@ const spon = [
     { img: spon06 },
     { img: spon07 },
     { img: spon08 },
+    { img: spon09 },
+    { img: spon10 },
+    { img: spon11 },
+    { img: spon12 },
+    { img: spon13 },
+    { img: spon14 },
+    { img: spon15 },
+    { img: spon16 },
+    { img: spon17 },
+    { img: spon18 },
+    { img: spon19 },
 ];
 </script>
 <style>
