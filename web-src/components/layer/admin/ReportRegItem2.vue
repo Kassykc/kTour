7b358c9b91a027132ff8d0a1371d19e425581d95
Sang Yee Park <design04@hicomp.net>
2025-05-26 17:44:59 +0900

    어드민 설문지 응답관리 팝업 레이아웃 수정

--- web-src/components/layer/admin/ReportRegItem2.vue
+++ web-src/components/layer/admin/ReportRegItem2.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="w-full flex flex-col justify-start items-start gap-[10px] p-[20px]">
+    <div class="w-full flex flex-col justify-start items-start p-[20px]">
 
 
-        <div class="w-full flex justify-start items-center gap-[10px]">
+        <div class="w-full flex justify-start items-center gap-[10px] border-t border-[#dcdcdc]">
             <label
-                class="flex justify-center items-center w-[70px] min-w-[70px] sm:w-[120px] sm:min-w-[120px] h-auto min-h-auto font-[600] border-r border-[#dcdcdc] bg-[#f5f5f5]">
+                class="flex justify-center items-center w-[70px] min-w-[70px] sm:w-[120px] sm:min-w-[120px] h-auto min-h-[30px] font-[600] border-r border-[#dcdcdc] bg-[#f5f5f5] py-[10px]">
                 응답필수여부
             </label>
             <label
@@ -17,30 +17,53 @@
             </label>
         </div>
 
-        <div class="w-full flex justify-start items-center gap-[10px]">
-            <div class="w-fit shrink-0">질문 :</div>
+        <div class="w-full flex justify-start items-center gap-[10px] border-t border-[#dcdcdc]">
+            <div class="w-fit shrink-0 flex justify-center items-center w-[70px] min-w-[70px] sm:w-[120px] sm:min-w-[120px] h-auto min-h-[30px] font-[600] border-r border-[#dcdcdc] bg-[#f5f5f5] py-[10px]">질문</div>
             <input v-model="props.modelValue.questionText.titleEn" type="text"
-                class="h-[36px] px-2 py-1 border border-[#dcdcdc] rounded bg-white w-full" placeholder="en" />
+                class="h-[30px] px-2 py-1 border border-[#dcdcdc] rounded bg-white w-full" placeholder="en" />
             <input v-model="props.modelValue.questionText.titleId" type="text"
-                class="h-[36px] px-2 py-1 border border-[#dcdcdc] rounded bg-white w-full" placeholder="id" />
+                class="h-[30px] px-2 py-1 border border-[#dcdcdc] rounded bg-white w-full" placeholder="id" />
         </div>
 
-        <select v-model="props.modelValue.answerType"
-            class="w-[150px] h-[36px] px-2 py-1 border border-[#dcdcdc] rounded bg-white text-sm">
-            <option value="1">단답식</option>
-            <option value="2">주관식</option>
-            <option value="3">단일선택형</option>
-            <option value="4">다중선택형</option>
-        </select>
+        <div class="flex justify-start items-center gap-[10px] border-t border-b border-[#dcdcdc] w-full">
+            <div class="w-fit shrink-0 flex justify-center items-center w-[70px] min-w-[70px] sm:w-[120px] sm:min-w-[120px] h-auto min-h-[30px] font-[600] border-r border-[#dcdcdc] bg-[#f5f5f5] py-[10px]">선택지 유형</div>
+            <select v-model="props.modelValue.answerType"
+            class="w-[150px] h-[30px] px-2 py-1 border border-[#dcdcdc] rounded bg-white text-sm">
+                <option value="1">단답식</option>
+                <option value="2">주관식</option>
+                <option value="3">단일선택형</option>
+                <option value="4">다중선택형</option>
+            </select>
+        </div>
 
-        <div class="flex flex-col justify-start items-center w-full gap-[10px]"
+        <div class="flex flex-col justify-start items-center w-full border-b border-[#dcdcdc]"
             v-for="(item, index) in props.modelValue.option" :key="index">
             <div class="w-full flex justify-start items-center gap-[10px]">
-                <div class="w-fit shrink-0">선택지 값 : </div>
-                <input type="text" class="h-[36px] px-2 py-1 border border-[#dcdcdc] rounded bg-white w-[50%]"
+                <div class="w-fit shrink-0 flex justify-center items-center w-[70px] min-w-[70px] sm:w-[120px] sm:min-w-[120px] h-auto min-h-[30px] font-[600] border-r border-[#dcdcdc] bg-[#f5f5f5] py-[10px]">선택지 값</div>
+                <input type="text" class="h-[30px] px-2 py-1 border border-[#dcdcdc] rounded bg-white w-[50%]"
                     placeholder="en" v-model="item.text.titleEn" />
-                <input type="text" class="h-[36px] px-2 py-1 border border-[#dcdcdc] rounded bg-white w-[50%]"
+                <input type="text" class="h-[30px] px-2 py-1 border border-[#dcdcdc] rounded bg-white w-[50%]"
                     placeholder="id" v-model="item.text.titleId" />
+
+
+                <!-- radio/checkbox -->
+                <div v-if="props.modelValue.answerType == '3' || props.modelValue.answerType == '4'"
+                    class="flex items-center justify-end w-[100px] gap-[10px]"
+                >
+                    <div v-if="props.modelValue.answerType == '3' || props.modelValue.answerType == '4'"
+                        class="radio_chkbox_area flex justify-start items-start gap-[10px] flex-wrap">
+                        <div class="w-full flex justify-start items-center gap-[10px] flex-wrap">
+                            <div class="h-[30px] leading-[26px] px-2 py-1 border border-[#dcdcdc] rounded cursor-pointer w-[42px]"
+                                @click="removeOption(index)">-</div>
+                        </div>
+                    </div>
+
+                    <div v-if="props.modelValue.answerType === '3' || props.modelValue.answerType === '4'"
+                        class="flex justify-center items-center gap-[10px]">
+                        <div class="h-[30px] leading-[26px] flex-1 px-2 py-1 border border-[#dcdcdc] rounded cursor-pointer w-[42px] text-white bg-black"
+                            @click="addOption(index)">+</div>
+                    </div>
+                </div>
             </div>
 
             <div class="w-full flex justify-start items-center gap-[10px] flex-wrap">
@@ -54,25 +77,18 @@
                     class="textarea_area flex justify-start items-start gap-[10px] flex-wrap w-full">
                 </div>
 
-                <!-- radio/checkbox -->
-                <div v-if="props.modelValue.answerType == '3' || props.modelValue.answerType == '4'"
-                    class="radio_chkbox_area flex justify-start items-start gap-[10px] flex-wrap w-[80%]">
-                    <div class="w-full flex justify-start items-center gap-[10px] flex-wrap">
-                        <div class="h-[36px] leading-[26px] px-2 py-1 border border-[#dcdcdc] rounded cursor-pointer w-[42px]"
-                            @click="removeOption(index)">-</div>
-                    </div>
-                </div>
-
-                <div v-if="props.modelValue.answerType === '3' || props.modelValue.answerType === '4'"
-                    class="flex justify-center items-center gap-[10px]">
-                    <div class="h-[36px] leading-[26px] flex-1 px-2 py-1 border border-[#dcdcdc] rounded cursor-pointer w-[42px] text-white bg-black"
-                        @click="addOption(index)">+</div>
-                </div>
+                
             </div>
         </div>
 
-        <div class="sub_area_delete_btn h-[36px] leading-[26px] flex-1 px-2 py-1 border border-[#dcdcdc] rounded cursor-pointer text-white bg-black"
-            @click="emit('remove')">삭제</div>
+        <div class="flex justify-end items-center w-full">
+            <div 
+                class="sub_area_delete_btn h-[36px] leading-[14px] w-[100px] px-2 py-[10px] border border-[#dcdcdc] rounded cursor-pointer text-white bg-black mt-[10px]"
+                @click="emit('remove')"
+            >
+                하위 질문 삭제
+            </div>
+        </div>
     </div>
 </template>
 <script setup lang="ts">
