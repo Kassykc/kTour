c0c5fae7482548b6b974c2017a97064349cb8158
Sang Yee Park <design04@hicomp.net>
2025-05-27 10:10:35 +0900

    파트너 로고 변경, 플로팅 메뉴 수정

--- web-src/components/common/floatingCard/index.vue
+++ web-src/components/common/floatingCard/index.vue
@@ -3,9 +3,9 @@
         class="floating fixed top-[64%] right-[27px] z-[20] md:top-[238px] md:right-[82px] flex flex-col gap-[14px] md:gap-[20px]">
         <!-- <CommonFloatingCardItem theme="pink" title="고객 전용 게시판" />
         <CommonFloatingCardItem theme="white" title="온라인견적/상담" /> -->
-        <CommonFloatingCardMobile theme="location" />
-        <CommonFloatingCardMobile theme="tel" />
-        <CommonFloatingCardMobile theme="mail" />
+        <CommonFloatingCardMobile to="/inquiry/contact" theme="location" />
+        <CommonFloatingCardMobile to="https://api.whatsapp.com/send?phone=6281188810374" theme="whats" />
+        <CommonFloatingCardMobile to="/inquiry/consultation" theme="mail" />
     </div>
 </template>
 
--- web-src/components/common/floatingCard/mobile/index.vue
+++ web-src/components/common/floatingCard/mobile/index.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="block">
-        <div @click="router.push('/')" class="cursor-pointer">
+        <div  class="cursor-pointer">
             <div class="floating flex flex-col justify-center items-center gap-[8px] w-[80px] h-[80px] rounded-full shadow-[0_3px_14px_rgba(0,0,0,0.25)]"
-                :class="[textClass, bgClass]">
+                :class="[textClass, bgClass]"
+                @click="movePage(to)"
+            >
                 <img class="w-[40px]" :src=iconSrc alt="아이콘 이미지">
             </div>
         </div>
@@ -12,7 +14,10 @@
 <script setup lang="ts">
 import iconLocation from "@/assets/images/location.png";
 import iconTel from "@/assets/images/tel.png";
+import iconWhats from "@/assets/images/whats.png";
 import iconMail from "@/assets/images/mail.png";
+
+
 
 const props = defineProps({
     to: {
@@ -33,8 +38,18 @@ const props = defineProps({
 
 const router = useRouter();
 
+const movePage = (page: string) => {
+    if (!page || page === '#') return
+    if (page.startsWith('http')) {
+        window.open(page, '_blank') // 외부 링크 새창
+    } else {
+        router.push(page) // 내부 라우팅
+    }
+}
+
 const iconSrc = computed(() => {
     if (props.theme === 'tel') return iconTel
+    if (props.theme === 'whats') return iconWhats
     if (props.theme === 'mail') return iconMail // 예시 아이콘
     return iconLocation // 기본
 })
@@ -44,6 +59,9 @@ const bgClass = computed(() => {
     }
     if (props.theme === 'mail') {
         return 'bg-[#5C8F3F]'
+    }
+    if (props.theme === 'whats') {
+        return 'bg-gradient-to-t from-[#25CF43] to-[#61FD7D]'
     }
     return 'bg-[#7A5083]'
 })
